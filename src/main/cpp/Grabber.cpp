4122fcff6f3c050db9555077603919e60ebcdc9a
William4813 <112589951+William4813@users.noreply.github.com>
2023-01-13 18:52:54 -0700

    Climber PID skeleton complete.

--- src/main/cpp/Grabber.cpp
+++ src/main/cpp/Grabber.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
 #include <Grabber.h>
-#include <frc/Solenoid.h>
+#include <frc/DoubleSolenoid.h>
 
 
-Grabber::Grabber(){}
+Grabber::Grabber(){
+
+        WristPIDInit();
+        WristDashInit();
+}
 
 Grabber::~Grabber(){}
+
+void Grabber::WristPIDInit() {
+ 
+    Grabber::WristDashInit();
+ 
+    m_wrist_PIDController.SetP(m_wrist_coeff.kP);
+    m_wrist_PIDController.SetI(m_wrist_coeff.kI);
+    m_wrist_PIDController.SetD(m_wrist_coeff.kD);
+    m_wrist_PIDController.SetIZone(m_wrist_coeff.kIz);
+    m_wrist_PIDController.SetFF(m_wrist_coeff.kFF);
+    m_wrist_PIDController.SetOutputRange(m_wrist_coeff.kMinOutput, m_wrist_coeff.kMaxOutput);
+ 
+}
+
+void Grabber::WristDashInit()
+{
+    frc::SmartDashboard::PutNumber("Wrist P Gain", m_wrist_coeff.kP);
+    frc::SmartDashboard::PutNumber("Wrist I Gain", m_wrist_coeff.kI);
+    frc::SmartDashboard::PutNumber("Wrist D Gain", m_wrist_coeff.kD);
+    frc::SmartDashboard::PutNumber("Wrist FF Gain", m_wrist_coeff.kFF);
+    frc::SmartDashboard::PutNumber("Wrist Max Output", m_wrist_coeff.kMaxOutput);
+    frc::SmartDashboard::PutNumber("Wrist Min Output", m_wrist_coeff.kMinOutput);
+ 
+}
+ 
+void Grabber::WristDashRead()
+{
+    m_wrist_coeff .kP = frc::SmartDashboard::GetNumber("Wrist P Gain", 0.0);
+    m_wrist_coeff .kI = frc::SmartDashboard::GetNumber("Wrist I Gain", 0.0);
+    m_wrist_coeff .kD = frc::SmartDashboard::GetNumber("Wrist D Gain", 0.0);
+    m_wrist_coeff .kFF = frc::SmartDashboard::GetNumber("Wrist FF Gain", 0.0);
+    m_wrist_coeff .kMinOutput = frc::SmartDashboard::GetNumber("Wrist Min Output", -1.0);
+    m_wrist_coeff .kMaxOutput = frc::SmartDashboard::GetNumber("Wrist Min Output", 1.0);
+ 
+}
--- src/main/include/Grabber.h
+++ src/main/include/Grabber.h
@@ -1,22 +1,35 @@
-#ifndef GRABBER_H_
-#define GRABBER_H_
-
-#include "rev/CANSparkMax.h"
-#include <frc/Encoder.h>
-#include "Constants.h"
 #include <iostream>
-#include <frc/Solenoid.h>
+#include <rev/CANSparkMax.h>
+#include <frc/DoubleSolenoid.h>
 #include <frc/Compressor.h>
+#include "Constants.h"
+#include "frc/smartdashboard/SmartDashboard.h"
 
-class Grabber 
-{
+
+class Grabber {
 
 public:
-  Grabber();
-  ~Grabber();
+
+        Grabber();
+        ~Grabber();
+        void WristPIDInit();
+        void WristDashInit();
+        void WristDashRead();
+        
+        rev::SparkMaxPIDController m_wrist_PIDController = m_wrist_motor.GetPIDController();
 
 private:
-  //Only needs one motor
-  rev::CANSparkMax m_motor_wrist{CONSTANTS::GRABBER::WRIST_MOTOR_ID, rev::CANSparkMax::MotorType::kBrushless};
+
+        frc::DoubleSolenoid m_grabberPiston{frc::PneumaticsModuleType::CTREPCM,0, 7};
+        
+       rev::CANSparkMax m_wrist_motor{CONSTANTS::GRABBER::WRIST_MOTOR_ID, rev::CANSparkMax::MotorType::kBrushless};
+
+
+       struct pidCoeff
+       {
+        double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
+        };
+
+        pidCoeff m_wrist_coeff{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+
 };
-#endif //GRABBER_H_
