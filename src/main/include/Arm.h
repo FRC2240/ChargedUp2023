1a9f3295e126a3326014b31bae3de3e6eb39df9b
Ayehed17 <112589877+Ayehed17@users.noreply.github.com>
2023-01-13 19:47:31 -0700

    Arm boilerplates

--- src/main/include/Arm.h
+++ src/main/include/Arm.h
@@ -4,13 +4,33 @@
 #include "ctre/Phoenix.h"
 #include <frc/Encoder.h>
 #include "Constants.h"
+#include "frc/smartdashboard/SmartDashboard.h"
 
 class Arm
 {
  public:
   Arm();
   ~Arm();
+  void Up();
+  void Down();
+  void ArmPIDInit();
+  void ArmDashInit();
+  void ArmDashRead();
+
+//Arm PID
 
  private:
+
+    WPI_TalonFX m_arm_motor_right {CONSTANTS::ARM::RIGHT_ARM_MOTOR_ID};
+    WPI_TalonFX m_arm_motor_left {CONSTANTS::ARM::LEFT_ARM_MOTOR_ID};
+
+    TalonFXSensorCollection m_Arm_RightEncoder = m_arm_motor_right.GetSensorCollection();
+
+    struct pidCoeff 
+    {
+        double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
+    };
+
+  pidCoeff m_Arm_RightCoeff{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 };
 #endif //ARM_H_
