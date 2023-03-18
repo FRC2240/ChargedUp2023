12edb8f4485764452c2d0c5cad86b55e3dd84856
Crosby <crosby.winfield@outlook.com>
2023-03-17 18:27:24 -0600

    Auto state machine

--- src/main/include/Robot.hpp
+++ src/main/include/Robot.hpp
@@ -77,7 +77,6 @@ public:
 private:
 
     CONSTANTS::STATES state = CONSTANTS::STATES::STORED;
-    CONSTANTS::AUTO_STATES auto_state;
     CONSTANTS::STATES last_state;
 
     bool fall_back_init = false;
@@ -129,7 +128,6 @@ private:
         kIntake,
         kScore,
         kBalance,
-        kIntake,
         kCSPath1,
         kCSPath2,
         kCSPath3,
@@ -153,10 +151,8 @@ private:
     };
 
     enum autoState {
-        kDriving,
-        kScoring,
         kIntaking,
-        kBalancing
+        kBalancing,
         kNothing
     };
 
